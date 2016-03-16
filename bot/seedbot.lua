e55dfe725343c10bf978c1dd84c6e8a9a0b2d617
samiiiiiiiiiiiiiiiiiiiiiiiiii <samijoni12345678@gmail.com>
2016-03-16 13:29:04 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -243,7 +243,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {122997453,0,tonumber(our_id)},--Sudo users
+    sudo_users = {122997453,123627901,134461890,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
