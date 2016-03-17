abb21a73a98184dd3665f50740d5e9e85e63428f
samiiiiiiiiiiiiiiiiiiiiiiiiii <samijoni12345678@gmail.com>
2016-03-17 11:43:02 -0700

    Update lock_badword.lua

--- plugins/lock_badword.lua
+++ plugins/lock_badword.lua
@@ -14,34 +14,44 @@ if msg.to.type == 'chat' then
     local chat = get_receiver(msg)
     local user = "user#id"..msg.from.id
     if lock_adds == "yes" then
-        send_large_msg(chat, '⛔️ کلمات زشت مجاز نمیباشد.')
+        send_large_msg(chat, '⛔️ badwords its not alowed here")
         chat_del_user(chat, user, ok_cb, true)
     end
 end
 end
 return {
   patterns = {
-    "(ک*س)$",
+    "[Kk][Ii][Rr]",
+    "[Kk][Oo][Ss]",
+    "[Nn][Aa][Nn][Aa][Tt]",
+    "[Nn][Aa][Nn][Ee]",
+    "[Hh][Aa][Rr][Oo][Mm]",
+    "[Kk][Oo][Nn]",
+    "[Kk][Oo][Oo][Nn]",
+    "[Dd][Aa][Hh][Aa][Nn]",
+    "[Dd][Aa][Yy][Oo][Ss]",
+    "[Dd][Aa][Uu][Ss]",
+    "[Kk][Oo][Ss][Kk][Ee][Ss][Hh]",
+    "[Jj][Ee][Nn][Dd][Ee]",
+    "[Kk][Aa][Rr][Ee][Tt]",
     "کیر",
-	"کص",
-	"جنده",
-	"حروم",
-	"کــــــــــــــــــــــــــــــیر",
-	"کـیـــــــــــــــــــــــــــــــــــــــــــــــــــر",
-    "ک×یر",
-	"ک÷یر",
-	"ک*ص",
-	"کــــــــــیرر",
-    "kir",
-	"kos",
-	"jende",
-	"گوساله",
-	"gosale",
-	"gusale"
-	"کونده"
-	"koonde"
-	"کوس"
-	"ممه"
+    "کس",
+    "کص",
+    "ننت",
+    "ننه",
+    "حروم",
+    "کون",
+    "دهن"
+    "دیوث"
+    "دیوس"
+    "دیوص"
+    "کسکش"
+    "کس کش"
+    "کص کش"
+    "کصکش"
+    "جنده"
+    "خارت"
+    "خوارت"
   },
   run = run
 }
