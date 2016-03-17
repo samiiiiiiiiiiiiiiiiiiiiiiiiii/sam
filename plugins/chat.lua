7fdca2d5ce762e42f2b7c7330f6d6961fdc792fb
samiiiiiiiiiiiiiiiiiiiiiiiiii <samijoni12345678@gmail.com>
2016-03-17 19:37:43 +0330

    Update chat.lua

--- plugins/chat.lua
+++ plugins/chat.lua
@@ -1,51 +1,123 @@
 local function run(msg)
 if msg.text == "hi" then
-	return "Hello bb"
+	return "Hi Baby :)"
 end
 if msg.text == "Hi" then
-	return "Hello honey"
+	return "Hello Honey :)"
 end
 if msg.text == "Hello" then
-	return "Hi bb"
+	return "Hi :)"
 end
 if msg.text == "hello" then
-	return "Hi honey"
+	return "Hi Honey"
 end
 if msg.text == "Salam" then
-	return "Salam aleykom"
+	return "Salam Azizam"
 end
 if msg.text == "salam" then
-	return "va aleykol asalam"
+	return "Salam"
 end
-if msg.text == "zac" then
-	return "Barash bezan sak"
+if msg.text == "Spam killer" then
+	return "Ha?"
 end
-if msg.text == "Zac" then
-	return "Barash bezan sak"
+if msg.text == "spam killer" then
+	return "Koft Chikaresh Dari?"
 end
-if msg.text == "ZAC" then
-	return "Barash bezan sak"
+if msg.text == "parsa" then
+	return "ba dadasham chikar dari ? :/"
 end
-if msg.text == "Bumper" then
-	return "Yes?"
+if msg.text == "Parsa" then
+	return "kooft chikaresh dari? :|"
 end
-if msg.text == "bumper" then
-	return "What?"
+if msg.text == "Hossein" then
+	return "hala amoom ro chikar dari bi namoos !"
 end
-if msg.text == "bot" then
-	return "hum?"
+if msg.text == "hossein" then
+	return "kooft ba amoom chikar dari ?"
 end
-if msg.text == "Bot" then
-	return "Huuuum?"
+if msg.text == "Navid" then
+	return "kooft ba babam chikar dari ?"
+end
+if msg.text == "navid" then
+	return "kooft ba babam chikar dari ?"
+end
+if msg.text == "sam" then
+	return "kooft ba dadash koochikam chikar dari ? man gheratiam 😡😡😡😡"
+end
+if msg.text == "Sam" then
+	return "kooft ba dadash koochikam chikar dari ?"
 end
 if msg.text == "?" then
-	return "Hum??"
+	return "Chiye?"
+end
+if msg.text == "؟" then
+	return "Kiiiiiir :|"
+end
+if msg.text == "BK" then
+	return "BK Ke BK 😐"
+end
+if msg.text == "Kir" then
+	return "Dos Dari?"
+end
+if msg.text == "kir" then
+	return "Dos Dari?"
 end
 if msg.text == "Bye" then
-	return "Bye Bye"
+	return "Bye Dayus"
 end
 if msg.text == "bye" then
-	return "Bye Bye"
+	return "Bye"
+end
+if msg.text == "سلام" then
+	return "سلام"
+end
+if msg.text == "سلام ربات" then
+	return "سلام عزیزم :)"
+end
+if msg.text == "ربات سلام" then
+	return "سلام عزیزم :)"
+end
+if msg.text == "پارسا" then
+	return "میگم بیاد جوابتو بده"
+end
+if msg.text == "حسین" then
+	return "عمو بیا کارت داره :|"
+end
+if msg.text == "ربات" then
+	return "ها چیه؟"
+end
+if msg.text == "سام" then
+	return "با جیگرم چیکار داری؟"
+end
+if msg.text == "نوید" then
+	return "رفته توالت وایسا بیاد"
+end
+if msg.text == "اسپم کیلر" then
+	return "ها چی می گی ؟"
+end
+if msg.text == "کله شیری" then
+	return "باباته کله کیییری 😐"
+end
+if msg.text == "بات" then
+	return "چیه چس ممبر 🤔"
+end
+if msg.text == "ربات" then
+	return "چیه چس ممبر 🤔"
+end
+if msg.text == "سید" then
+	return "چچچ سیک"
+end
+if msg.text == "تله سید" then
+	return "چچچ سیک"
+end
+if msg.text == "خوبی" then
+	return "شکر خدا شما خوبی"
+end
+if msg.text == "خوبی؟" then
+	return "مرسی عزیزم تو خوبی؟"
+end
+if msg.text == "بات خوبی؟" then
+	return "مرسی 😁"
 end
 end
 
@@ -53,15 +125,41 @@ return {
 	description = "Chat With Robot Server", 
 	usage = "chat with robot",
 	patterns = {
-		"^[Hh]i$",
-		"^[Hh]ello$",
-		"^[Zz]ac$",
-		"^ZAC$",
-		"^[Bb]ot$",
-		"^[Uu]mbrella$",
-		"^[Bb]ye$",
-		"^?$",
-		"^[Ss]alam$",
+    "^[Hh]i$",
+	"^[Hh]ello$",
+    "^[Ss]alam$",
+    "^Spam killer$",
+    "^spam killer$",
+    "^Parsa$",
+    "^parsa$",
+    "^hossein$",
+    "^BK$",
+    "^[Kk]ir$",
+	"^$",
+	"^[Bb]ye$",
+	"^سلام",
+	"^Hossein$",
+    "^Navid$",
+	"^navid$",
+	"^Sam$",	
+	"^sam$",
+	"^سلام ربات$",
+	"^ربات سلام$",
+	"^نوید$",
+	"^اسپم کیلر$",
+	"^کله شیری",
+	"^پارسا$",	
+	"^بات",
+	"^ربات",
+	"^سید",
+	"^تله سید",
+	"^خوبی",
+	"^خوبی؟",
+	"^ربات",
+	"^سام",
+	"^بات خوبی؟",
+	"^حسین",
+		
 		}, 
 	run = run,
     --privileged = true,
