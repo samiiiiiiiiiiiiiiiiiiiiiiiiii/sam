fb3a3b84130f5a783dd49f4286ff1f063f060fa0
samiiiiiiiiiiiiiiiiiiiiiiiiii <samijoni12345678@gmail.com>
2016-03-17 03:33:37 -0700

    Update info..lua

--- plugins/info..lua
+++ plugins/info..lua
@@ -86,7 +86,7 @@ local function action_by_id(extra, success, result)  -- /info <ID> function
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'Total messages : '..user_info_msgs..'\n\n'
-  text = text..'@SBSS_Team'
+  text = text..'@samramezani'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
   send_msg(extra.receiver, 'id not found.\nuse : /info @username', ok_cb, false)
